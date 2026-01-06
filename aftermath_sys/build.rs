2d63a3aeb67b27b3868d6bf5724328482c6171c5
ErikWDev <ErikWDev@gmail.com>
2026-01-06 15:36:31 +0100

    add linux .so files as well

--- aftermath_sys/build.rs
+++ aftermath_sys/build.rs
@@ -1,15 +1,69 @@
+pub fn copy_dir_all(
+    src: impl AsRef<std::path::Path>,
+    dst: impl AsRef<std::path::Path>,
+) -> std::io::Result<()> {
+    std::fs::create_dir_all(&dst)?;
+    for entry in std::fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            std::fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(())
+}
+
 fn main() {
     println!("cargo:rerun-if-changed=./build.rs");
 
-    // println!("cargo:rustc-llink-search=./vendor/current/lib/x86/");
-    println!("cargo:rustc-llink-search=./vendor/current/lib/x64/");
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let from_lib_path = std::path::PathBuf::from("./vendor/current/lib")
+        .canonicalize()
+        .unwrap();
+    let to_lib_path = out_path.join("lib");
+    let _ = copy_dir_all(from_lib_path, &to_lib_path).unwrap();
+
     println!("cargo:rustc-link-lib={}", lib_name());
+
+    #[cfg(target_arch = "x86_64")]
+    {
+        println!(
+            "cargo:rustc-link-search={}",
+            to_lib_path.join("x64").display()
+        );
+    };
+    #[cfg(target_arch = "x86")]
+    {
+        println!(
+            "cargo:rustc-link-search={}",
+            to_lib_path.join("x86").display()
+        );
+    };
 
     /*
         NOTE: Currently commented out as I switched to shipping pre-generated bindings
               See `./generate.sh`
     */
     // generate_bindings();
+}
+
+#[allow(unreachable_code)]
+pub const fn lib_name() -> &'static str {
+    #[cfg(target_arch = "x86_64")]
+    {
+        return "GFSDK_Aftermath_Lib.x64";
+    };
+
+    #[cfg(target_arch = "x86")]
+    {
+        return "GFSDK_Aftermath_Lib.x86";
+    };
+
+    {
+        panic!();
+    };
 }
 
 // pub fn generate_bindings() {
@@ -31,22 +85,3 @@ fn main() {
 //         .write_to_file(out_path.join("bindings.rs"))
 //         .unwrap();
 // }
-
-pub const fn lib_name() -> &'static str {
-    #[cfg(windows)]
-    {
-        // return "GFSDK_Aftermath_Lib.x86.dll";
-        return "GFSDK_Aftermath_Lib.x64.dll";
-    };
-
-    #[cfg(unix)]
-    {
-        // return "GFSDK_Aftermath_Lib.x86.lib";
-        return "GFSDK_Aftermath_Lib.x64.lib";
-    };
-
-    #[allow(unreachable_code)]
-    {
-        panic!();
-    };
-}
